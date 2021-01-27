e6a053124aa2ebfac840279a363d9723d4c171e0
VladimirLGit <1077control@gmail.com>
2021-01-27 23:48:14 +0300

    Task4

--- src/eu/senla/task3/Main.java
+++ src/eu/senla/task3/Main.java
@@ -3,7 +3,7 @@ package eu.senla.task3;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Random;
-import java.util.Set;
+
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/eu/senla/task4/Main.java
@@ -0,0 +1,7 @@
+package eu.senla.task4;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
