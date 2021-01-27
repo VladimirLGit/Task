c5447b915a8f05deabcdd41220a3b0f7adbe90a2
VladimirLGit <1077control@gmail.com>
2021-01-28 00:31:44 +0300

    Task4 print Matrix

--- src/eu/senla/task4/Main.java
+++ src/eu/senla/task4/Main.java
@@ -2,6 +2,8 @@ package eu.senla.task4;
 
 public class Main {
     public static void main(String[] args) {
-
+        Matrix m = new Matrix();
+        m.createMatrix();
+        m.printMatrix();
     }
 }
--- src/eu/senla/task4/Matrix.java
+++ src/eu/senla/task4/Matrix.java
@@ -16,19 +16,66 @@ package eu.senla.task4;
 7. Вывести на экран первоначальную матрицу.
 */
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Matrix {
+    private final String ANSI_RESET = "\u001B[0m";
+    private final String ANSI_BLACK = "\u001B[30m";
+    private final String ANSI_RED = "\u001B[31m";
+    private final String ANSI_GREEN = "\u001B[32m";
+    private final String ANSI_YELLOW = "\u001B[33m";
+    private final String ANSI_BLUE = "\u001B[34m";
+    private final String ANSI_PURPLE = "\u001B[35m";
+    private final String ANSI_CYAN = "\u001B[36m";
+    private final String ANSI_WHITE = "\u001B[37m";
+
     private String strChar = "FIDrwGgpAoEQsUgpIoWnpRnLnfEd";
-    public double randomDouble(){
-       return Math.random() * 100;
+    private String[][] strMatrix;
+
+    public String randomDouble(){
+       return new DecimalFormat("000.00").format( Math.random() * 1000);
     }
+
     public String randomString(){
         StringBuilder sb = new StringBuilder();
         Random rand = new Random();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             sb.append(strChar.charAt(rand.nextInt(strChar.length())));
         }
         return sb.toString();
     }
+
+    public void createMatrix(){
+        int index = 0;
+        strMatrix = new String[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                strMatrix[i][j] = index==0 ? randomDouble() : randomString();
+                index++;
+                if (index>2){
+                    index = 0;
+                }
+            }
+        }
+    }
+    public void printMatrix(){
+        int index = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (index==0) {
+                    System.out.print(ANSI_GREEN + strMatrix[i][j] + ANSI_RESET + " ");
+                }
+                else{
+                    System.out.print(strMatrix[i][j] + " ");
+                }
+                index++;
+                if (index>2){
+                    index = 0;
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
