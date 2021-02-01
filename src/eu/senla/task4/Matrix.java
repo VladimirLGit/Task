a38901a6406ce0eaa918c41173312583a1b76446
VladimirLGit <77676117+VladimirLGit@users.noreply.github.com>
2021-02-01 16:56:32 +0300

    fix index = -1;

--- src/eu/senla/task4/Matrix.java
+++ src/eu/senla/task4/Matrix.java
@@ -1,3 +1,7 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 package eu.senla.task4;
 
 /*
@@ -100,7 +104,7 @@ public class Matrix {
         parserDiagonal(strMainDiagonal);
         parserDiagonal(strReversDiagonal);
         int index = sb.lastIndexOf(",");
-        sb.deleteCharAt(index);
+        if (index != -1) sb.deleteCharAt(index);
     }
 
 
