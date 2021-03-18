39d9b1fc3627d077d9366d400ec1e9f2fdb089b3
VladimirLGit <1077control@gmail.com>
2021-03-19 00:30:30 +0300

    Task10

---
+++ src/eu/senla/task10/Main.java
@@ -0,0 +1,12 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+package eu.senla.task10;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
