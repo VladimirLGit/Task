a225e2cb45b5bef0baede3a24e0728d054e02b81
VladimirLGit <1077control@gmail.com>
2021-04-14 00:47:01 +0300

    readAll; readList; createTable;

--- src/eu/senla/task12/Connector.java
+++ src/eu/senla/task12/Connector.java
@@ -4,39 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Connector {
+    private final String driver = "com.mysql.jdbc.Driver";
     private final String USERNAME = "root";
     private final String PASSWORD = "root";
     private final String DB_URL = "jdbc:mysql://127.0.0.1:3306/users";
-    private Connection connection = null;
+    //private Connection connection = null;
     public PreparedStatement preparedStatement = null;
 
-    public Connector() throws ClassNotFoundException {
-        // Register JDBC driver
-        Class.forName("com.mysql.jdbc.Driver");
-    }
-
-    private Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-    }
-
-    public boolean createTableUsers(){
+    public Connection getConnection() {
+        Properties props = new Properties();
+        Connection con = null;
         try {
-            connection = getConnection();
-            preparedStatement = connection.prepareStatement(
-                    "CREATE TABLE users (\n" +
-                            " id int(10) NOT NULL AUTO_INCREMENT,\n" +
-                            " login varchar(20) NOT NULL,\n" +
-                            " password varchar(24) NOT NULL,\n" +
-                            " PRIMARY KEY (id)\n" +
-                            ");");
-            boolean execute = preparedStatement.execute();
-            connection.close();
-            return execute;
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-            return false;
+            // load the Driver Class
+            props.setProperty("password"         , PASSWORD);
+            props.setProperty("user"             , USERNAME);
+            props.setProperty("url"              , DB_URL  );
+            props.setProperty("useUnicode"       , "true"  );
+            props.setProperty("characterEncoding", "utf8"  );
+            Class.forName(driver);
+
+            // create the connection now
+            con = DriverManager.getConnection(props.getProperty("url"),
+                    props.getProperty("user"),
+                    props.getProperty("password"));
+        } catch (ClassNotFoundException | SQLException e) {
+             e.printStackTrace();
         }
+        return con;
     }
 }
--- src/eu/senla/task12/users/DaoUserImpl.java
+++ src/eu/senla/task12/users/DaoUserImpl.java
@@ -3,17 +3,35 @@ package eu.senla.task12.users;
 import eu.senla.task12.Connector;
 import eu.senla.task12.Dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DaoUserImpl implements Dao {
     private Connector conn;
 
-    public DaoUserImpl() {
-        try {
-            conn = new Connector();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+    public boolean createTableUsers(){
+        final String QUERY ="CREATE TABLE users (\n" +
+                " id int(10) NOT NULL AUTO_INCREMENT,\n" +
+                " login varchar(20) NOT NULL,\n" +
+                " password varchar(24) NOT NULL,\n" +
+                " PRIMARY KEY (id)\n" +
+                ");";
+        try(Connection con = conn.getConnection();
+            Statement stmt = con.createStatement();) {
+            boolean execute = stmt.execute(QUERY);
+            return execute;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return false;
         }
+    }
+
+    public DaoUserImpl() {
+        conn = new Connector();
     }
 
     @Override
@@ -43,6 +61,39 @@ public class DaoUserImpl implements Dao {
 
     @Override
     public List<User> readList() {
-        return null;
+        List<User> users = new ArrayList<>();
+        final String QUERY = "select id,login,password from Users";
+        try(Connection con = conn.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY)) {
+
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("login");
+                String password = rs.getString("password");
+                users.add(new User(name, password));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return users;
+    }
+
+    public void readAllUsers(){
+        final String QUERY = "select id,login,password from Users";
+        try(Connection con = conn.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY)) {
+
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("login");
+                String password = rs.getString("password");
+                System.out.println(id + "," +name+ "," +password);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
